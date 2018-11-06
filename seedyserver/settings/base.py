93d6d548da4f0ba49d4c75fed14f7f5d292225a1
Jack Rose <jackderryrose@gmail.com>
2018-11-06 23:33:37 +1100

    Setup changes for prod

--- seedyserver/settings/base.py
+++ seedyserver/settings/base.py
@@ -22,11 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mwitn_znsdwkir37xbh#vgx501vtep$#ue575mduyg5mu+qr8%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
