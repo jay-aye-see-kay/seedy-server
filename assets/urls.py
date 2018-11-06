893e0c72f173e86c5fa1755df416b55cac116e2d
Jack Rose <jackderryrose@gmail.com>
2018-11-06 16:16:24 +1100

    Switch to a more useful options endpoint

--- assets/urls.py
+++ assets/urls.py
@@ -4,9 +4,7 @@ from assets import views
 
 router = DefaultRouter()
 router.register(r'assets', views.AssetViewSet)
-router.register(r'assettypes', views.AssetTypeViewSet)
-router.register(r'brands', views.BrandViewSet)
-router.register(r'locations', views.LocationViewSet)
+router.register(r'options', views.OptionsViewSet, basename='options')
 # router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
--- assets/views.py
+++ assets/views.py
@@ -3,6 +3,7 @@ from assets.models import Asset, AssetType, Brand, Location
 from assets.serializers import AssetSerializer, AssetTypeSerializer, BrandSerializer, LocationSerializer
 from rest_framework import generics, permissions, renderers, viewsets
 from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
 
 
 class AssetViewSet(viewsets.ModelViewSet):
@@ -10,16 +11,14 @@ class AssetViewSet(viewsets.ModelViewSet):
     serializer_class = AssetSerializer
 
 
-class AssetTypeViewSet(viewsets.ModelViewSet):
-    queryset = AssetType.objects.all()
-    serializer_class = AssetTypeSerializer
+class OptionsViewSet(viewsets.ViewSet):
+    def list(self, request):
+        asset_types = AssetTypeSerializer(AssetType.objects.all(), many=True)
+        brands = BrandSerializer(Brand.objects.all(), many=True)
+        locations = LocationSerializer(Location.objects.all(), many=True)
 
-
-class BrandViewSet(viewsets.ModelViewSet):
-    queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
-
-
-class LocationViewSet(viewsets.ModelViewSet):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
+        return Response({
+            'asset_types': asset_types.data,
+            'brands': brands.data,
+            'locations': locations.data,
+        })
