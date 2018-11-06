86f4ae17fe345435ae75353947841b8df8479623
Jack Rose <jackderryrose@gmail.com>
2018-11-06 15:51:59 +1100

    Allow all hosts for local dev (change before prod)

--- seedyserver/settings.py
+++ seedyserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mwitn_znsdwkir37xbh#vgx501vtep$#ue575mduyg5mu+qr8%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
